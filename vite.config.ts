03aef08ea80ff7ff60e47137a32f8e88eb282716
xionkq <3030199850@qq.com>
2024-01-22 21:41:05 +0800

    try mpa

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  // plugins: [vue()],
+  // npm run --project=one
+  // npm build --project=one
+  const npm_config_project = process.env['npm_config_project']
+  if (!npm_config_project) {
+    throw new Error(
+      '缺少指定模块!, 如果是构建指定模块，请使用 --project=[module_name]'
+    )
+  }
+  // 入口文件
+  const input = resolve(__dirname, `src/${npm_config_project}/index.html`)
+
   return {
-    // 设置项目根目录
-    root: './otherRoot',
-    // public服务路径
-    base: '/',
-    // 会覆盖构建时的模式
-    mode: 'development',
+    root: `./src/${npm_config_project}`,
+    build: {
+      rollupOptions: {
+        input,
+        output: {
+          dir: `dist/${npm_config_project}`
+        }
+      }
+    },
     plugins: [vue()],
   }
 })
